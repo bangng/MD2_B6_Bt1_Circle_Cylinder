ac028c46a976d040ac3bf9ab27ed60cadae89290
bangng <montersb@gmail.com>
2022-08-01 16:31:36 +0700

    bang

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10,"red");
+        System.out.println(circle);
+        Cylinder cylinder = new Cylinder(12,"blue",8);
+        System.out.println(cylinder);
+
+    }
+}
